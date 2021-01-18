57a24c7026496b7bfa9b6d3e35ca7c0129e7dcac
danielmc19 <dmonestel@gbm.net>
2021-01-18 09:40:31 -0600

    Project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductFilterPipe } from './pipes/product-filter.pipe';
+import { EditProductDialogComponent } from './components/edit-product-dialog/edit-product-dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductItemComponent,
+    HomeComponent,
+    ProductFilterPipe,
+    EditProductDialogComponent,
+    DeleteDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/Product.ts
@@ -0,0 +1,8 @@
+export default class Product {
+    id: string;
+    name: string;
+    category_name: string;
+    price: number;
+    quantity: number;
+    status: string;
+}
